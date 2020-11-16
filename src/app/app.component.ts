9e1aeff84cc8d141d4593f570282ed40b2608b1e
vishnu <hkantvishnu@gmail.com>
2020-11-16 14:33:26 +0530

    Add navigation side bar footer and provide roting to home and about us page in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,17 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   title = 'formApp';
  
+
+  openNav() {
+    document.getElementById("mySidenav").style.width = "15vw";
+    document.getElementById("aside").style.width="15vw";
+   document.getElementById("section").style.width="40vw";
+  }
   
+  closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("aside").style.width="3vw";
+    document.getElementById("aside").style.height="10vh"
+    document.getElementById("section").style.width="100%";
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,22 @@ import {FormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormPageComponent } from './form-page/form-page.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HomeComponent } from './home/home.component';
 
 
 const appRoutes:Routes=[
-  {path:'',component:FormPageComponent},
+  {path:'',component:HomeComponent},
+  {path:'about-us',component:AboutUsComponent},
+  {path:'home',component:HomeComponent},
   {path:'userInfo',component:UserInfoComponent}
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormPageComponent
+    FormPageComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
